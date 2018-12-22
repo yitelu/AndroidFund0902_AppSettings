5bc77cd7426124879635c21a20f500ed596c04d9
Richard Yi-Te Lu <yitelu@gmail.com>
2018-12-22 12:44:20 -0800

    Android App Settings

---
+++ app/src/main/java/com/ytl/appwithsettings/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.ytl.appwithsettings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static final String KEY_PREF_EXAMPLE_SWITCH = "example_switch";
+}
